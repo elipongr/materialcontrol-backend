0f4b71a8cc6c9b42743be59a4d6df810fae42787
Elipongr <pelipongracz@gmail.com>
2020-12-29 02:48:39 +0100

    Initial commit

---
+++ src/main/java/com/elipongr/materialcontrolbackend/MaterialcontrolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elipongr.materialcontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialcontrolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialcontrolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elipongr/materialcontrolbackend/MaterialcontrolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elipongr.materialcontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialcontrolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
